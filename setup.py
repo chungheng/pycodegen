2d16544db77e3455fe0304f2fbdb40a999a371b9
chungheng <chungheng.yeh@gmail.com>
2018-05-11 14:17:56 -0400

    fix jinja release number

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ if __name__ == "__main__":
         maintainer_email = MAINTAINER_EMAIL,
         packages=PACKAGES,
         install_requires=[
-            'jinja >= 2.8'
+            'jinja2 >= 2.8'
         ]
     )
